afb7447cd789db809ded8834722e15e5c09daf35
razalas340 <135702134+razalas340@users.noreply.github.com>
2023-09-19 08:49:17 -0400

    check prefix of a word approach

--- Algo-Training.js
+++ Algo-Training.js
@@ -176,4 +176,57 @@ var findColumnWidth = function(grid) {
   // Output: 2
   // Explanation: "pro" is prefix of "problem" which is the 2nd and the 6th word in the sentence, but we return 2 as it's the minimal index.
   
+  // 1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence
+// Easy
+// 778
+// 37
+// Companies
+// Given a sentence that consists of some words separated by a single space, and a searchWord, check if searchWord is a prefix of any word in sentence.
+
+// Return the index of the word in sentence (1-indexed) where searchWord is a prefix of this word. If searchWord is a prefix of more than one word, return the index of the first word (minimum index). If there is no such word return -1.
+
+// A prefix of a string s is any leading contiguous substring of s.
+
+// input: sentence, searchWord
+// output: index of the minimum word where the prefix is
+var isPrefixOfWord = function(sentence, searchWord) {
+    let minimum = 0;
+    const words = sentence.split(' ');
+    console.log(words);
+    for (let i = 0; i < words.length; i++) {
+      if (words[i].includes(searchWord)) {
+        minimum = i;
+      }
+    }
+    return -1;
+  }
+  
+  // const sentence = "i love eating burger";
+  // const searchWord = "burg";
+  
+  // const sentence2 = "hellohello hellohellohello"
+  // const searchWord2 = "ell"
+  
+  const sentence1 = "this problem is an easy problem"
+  const searchWord1 = "pro"
+  
+  // console.log(isPrefixOfWord(sentence, searchWord));
+  console.log(isPrefixOfWord(sentence1, searchWord1));
+  // console.log(isPrefixOfWord(sentence2, searchWord2));
+  
+  
+  
+   
+  
+  // Example 1:
+  
+  // Input: sentence = "i love eating burger", searchWord = "burg"
+  // Output: 4
+  // Explanation: "burg" is prefix of "burger" which is the 4th word in the sentence.
+  // Example 2:
+  
+  // Input: sentence = "this problem is an easy problem", searchWord = "pro"
+  // Output: 2
+  // Explanation: "pro" is prefix of "problem" which is the 2nd and the 6th word in the sentence, but we return 2 as it's the minimal index.
+  
   
