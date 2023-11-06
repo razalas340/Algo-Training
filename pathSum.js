8f2fdad35b40dee9788005e7ed13ea9f4f84ab47
razalas340 <135702134+razalas340@users.noreply.github.com>
2023-11-06 08:32:16 -0500

    algo and solution

---
+++ pathSum.js
@@ -0,0 +1,70 @@
+// 112. Path Sum
+// Solved
+// Easy
+// Topics
+// Companies
+// Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+// A leaf is a node with no children.
+// https://leetcode.com/problems/path-sum/
+// * function TreeNode(val, left, right) {
+//  *     this.val = (val===undefined ? 0 : val)
+//  *     this.left = (left===undefined ? null : left)
+//  *     this.right = (right===undefined ? null : right)
+//  * }
+//  */
+
+var hasPathSum = function (root, targetSum) {
+  let result = false;
+  dfs(root, 0);
+  return result;
+
+  function dfs(node, curSum) {
+    if (!node) return;
+
+    curSum += node.val;
+
+    if (!node.left && !node.right && curSum === targetSum) {
+      result = true;
+    }
+
+    dfs(node.left, curSum);
+    dfs(node.right, curSum);
+  }
+};
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+// Create nodes
+const root = new TreeNode(5);
+root.left = new TreeNode(4);
+root.right = new TreeNode(8);
+root.left.left = new TreeNode(11);
+root.left.left.left = new TreeNode(7);
+root.left.left.right = new TreeNode(2);
+root.right.left = new TreeNode(13);
+root.right.right = new TreeNode(4);
+root.right.right.right = new TreeNode(1);
+
+// Target sum
+const targetSum = 22;
+
+console.log(hasPathSum(root, targetSum)); // Output should be true
+
+// Example 1:
+
+// Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+// Output: true
+// Explanation: The root-to-leaf path with the target sum is shown.
+// Example 2:
+
+// Input: root = [1,2,3], targetSum = 5
+// Output: false
+// Explanation: There two root-to-leaf paths in the tree:
+// (1 --> 2): The sum is 3.
+// (1 --> 3): The sum is 4.
+// There is no root-to-leaf path with sum = 5.
