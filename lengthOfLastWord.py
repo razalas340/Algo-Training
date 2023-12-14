60d928760ef8f234b0e940fdb8f150778252088e
razalas340 <135702134+razalas340@users.noreply.github.com>
2023-12-14 08:54:21 -0500

    python solution

---
+++ lengthOfLastWord.py
@@ -0,0 +1,12 @@
+class Solution:
+  def lengthOfLastWord(self, s: str) -> int:
+    total = 0
+
+    for i in range(len(s), -1, -1):
+      if total > 0 and s[i] == ' ':
+        return total
+
+      if s[i] != ' '
+          total += 1
+
+    return total 
