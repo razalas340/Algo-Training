7bcb41366734bdda533ef5d6e04b73358b44cfd6
razalas340 <135702134+razalas340@users.noreply.github.com>
2023-12-14 08:55:37 -0500

    missing start fix

--- lengthOfLastWord.py
+++ lengthOfLastWord.py
@@ -2,7 +2,7 @@ class Solution:
   def lengthOfLastWord(self, s: str) -> int:
     total = 0
 
-    for i in range(len(s), -1, -1):
+    for i in range(len(s) -1 , -1, -1):
       if total > 0 and s[i] == ' ':
         return total
 
